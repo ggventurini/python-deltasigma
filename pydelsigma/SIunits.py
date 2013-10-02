9880da7b8073bef141c3eb64db0d6711b758d246
ggventurini <ggventurini+GITHUB@gmail.com>
2013-10-02 18:18:40 +0200

    Initial commit.

---
+++ pydelsigma/SIunits.py
@@ -0,0 +1,49 @@
+from __future__ import division
+import numpy as np 
+import pydelsigma
+import pydelsigma.constants
+from pydelsigma.constants import eps
+
+def SIunits(x):
+	""" The factors and suffixes supported are:
+ 1e-3	m	milli		1e3	k	kilo
+ 1e-6	u	micro		1e6	M	mega
+ 1e-9	n	nano		1e9	G	giga
+ 1e-12	p	pico		1e12	T	tera
+ 1e-15	f	femto		1e15	P	peta
+ 1e-18	a	atto		1e18	E	exa			
+ 1e-21	z	zepto		1e21	Z	zeta
+ 1e-24 y	yocto		1e24	Y	yotta"""
+	prefixes_n = ('', 'm', 'u', 'n', 'p', 'f', 'a', 'z', 'y')
+	prefixes_p = ('', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+	factor = np.ones(len(x))
+	prefix = ['']*len(x)
+	for i in range(len(x)):
+    		if x[i] != 0:
+		        p = int(np.floor(np.log10(abs(x[i]))/3 + eps))
+		else:
+		        p = 0
+		if p >= 0:
+			p = min(p, len(prefixes_p))
+			prefix[i] = prefixes_p[p]
+			factor[i] = 10**(3*p)
+		elif p < 0:
+			p = min(-p, len(prefixes_n)-1)
+			prefix[i] = prefixes_n[p]
+			factor[i] = 10**(-3*p)
+		#else:
+		#	prefix[i] = ''
+		#	factor[i] = 1
+	return factor, prefix
+
+def test():
+	tv = (1e3, 2100312.24, .32545, 21e-9, 34e-12, 9569300e-12)
+	correct = (1e3, 'k'), (1e6, 'M'), (1e-3, 'm'), (1e-9, 'n'), (1e-12, 'p'), (1e-6, 'u')
+	f, p = SIunits(tv)
+	res = zip(f,p)
+	for r, c in zip(res, correct):
+		assert r[0] == c[0] and r[1] == c[1]
+	print "Test passed."
+
+if __name__ == '__main__':
+	test()
