653caf1976c1b6fa9de755ab97fbab6b3ba191a5
ggventurini <ggventurini@gmail.com>
2013-10-06 15:20:16 +0200

    Add missing trailing colon.

--- pydelsigma/zinc.py
+++ pydelsigma/zinc.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def zinc(f, m=64, n=1)
+def zinc(f, m=64, n=1):
 	""" mag = zinc(f, m=64, n=1)	
 	Calculate the magnitude response of a cascade of n mth-order 
 	comb filters at frequencies f.
