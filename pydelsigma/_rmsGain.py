a8620cd166e01aeea0ae6cba807486caf0dd1323
ggventurini <ggventurini@gmail.com>
2013-10-16 15:31:52 +0200

    Initial commit.

---
+++ pydelsigma/_rmsGain.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# _rmsGain.py
+# Module providing the rmsGain function
+# Copyright 2013 Giuseppe Venturini
+# This file is part of python-deltasigma.
+#
+# python-deltasigma is a 1:1 Python replacement of Richard Schreier's 
+# MATLAB delta sigma toolbox (aka "delsigma"), upon which it is heavily based.
+# The delta sigma toolbox is (c) 2009, Richard Schreier.
+#
+# python-deltasigma is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# LICENSE file for the licensing terms.
+
+"""Module providing the rmsGain() function
+"""
+
+import numpy as np
+from scipy.linalg import norm
+from ._evalTF import evalTF
+
+def rmsGain(H, f1, f2, N=100):
+	"""function g = rmsGain(H, f1, f2, N=100)
+	Compute the root mean-square gain of the discrete-time
+	tf H in the frequency band (f1, f2).
+	"""
+
+	w = np.linspace(2*np.pi*f1, 2*np.pi*f2, N)
+	g = norm(evalTF(H, np.exp(1j*w))) / np.sqrt(N)
+
+	return g
+	
+def test_rmsGain():
+	from ._utils import empty
+	H = empty()
+	H.num = (1,)
+	H.den = (1, 2, 10)
+	f1 = 0.001
+	f2 = 0.5
+	res = rmsGain(H, f1, f2, N=1000)
+	res1 = 0.102245275091
+	assert np.allclose((res,), (res1,), rtol=1e-05, atol=1e-08)
+	
+	
+if __name__ == '__main__':
+	test_rmsGain()
