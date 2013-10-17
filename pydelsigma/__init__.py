a486d9338e4950b97e57d7dc67a228ca6b7b1514
ggventurini <ggventurini@gmail.com>
2013-10-17 21:01:49 +0200

    Add missing modules to import.

--- pydelsigma/__init__.py
+++ pydelsigma/__init__.py
@@ -34,7 +34,9 @@ from ._delay import delay
 from ._ds_f1f2 import ds_f1f2
 from ._ds_freq import ds_freq
 from ._ds_hann import ds_hann
+from ._ds_optzeros import ds_optzeros
 from ._ds_quantize import ds_quantize
+from ._ds_synNTFobj1 import ds_synNTFobj1
 from ._evalRPoly import evalRPoly
 from ._evalTF import evalTF
 from ._impL1 import impL1
@@ -44,6 +46,7 @@ from ._padb import padb
 from ._padl import padl
 from ._padr import padr
 from ._padt import padt
+from ._plotPZ import plotPZ
 from ._pulse import pulse
 from ._rms import rms
 from ._sinc_decimate import sinc_decimate
@@ -51,5 +54,5 @@ from ._thermometer import thermometer
 from ._undbm import undbm
 from ._undbp import undbp
 from ._undbv import undbv
-from ._utils import mfloor, rat, gcd, lcm, db
+from ._utils import cplxpair, mfloor, rat, gcd, lcm, db
 from ._zinc import zinc
