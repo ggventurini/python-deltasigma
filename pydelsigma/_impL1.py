787f710fd9c69d1b1814f2892327f5c3697fa85d
ggventurini <ggventurini+GITHUB@gmail.com>
2013-11-10 13:11:58 +0100

    Pass unit test again.

--- pydelsigma/_impL1.py
+++ pydelsigma/_impL1.py
@@ -50,24 +50,25 @@ def impL1(arg1, n=10):
 	lf_num = lf_den - den
 	ts = np.arange(n)
 	all_lf = np.concatenate((lf_num, lf_den), axis=1)
+	lf_num, lf_den = lf_num.squeeze(), lf_den.squeeze()
 	if not np.allclose(np.imag(all_lf), np.zeros(all_lf.shape), atol=1e-9):
 		# Complex loop filter
 		lfr_den = np.real(conv(lf_den, np.conj(lf_den))).squeeze()
 		lfr_num = conv(lf_num, np.conj(lf_den)).squeeze()
 		lf_i = (np.real(lfr_num).tolist()[0], lfr_den.tolist()[0], 1)
 		lf_q = (np.imag(lfr_num).tolist()[0], lfr_den.tolist()[0], 1)
-		y = dimpulse(lf_i, t=ts) + 1j*dimpulse(lf_q, t=ts)
+		_, y = dimpulse(lf_i, t=ts) + 1j*dimpulse(lf_q, t=ts)
 	else:
-		y = dimpulse((lf_num, lf_den, 1), t=ts)
-	return y
+		_, y = dimpulse((lf_num, lf_den, 1), t=ts)
+	return y[0].squeeze()
 
 def test_impL1():
 	"""Test function for impL1()"""
 	sys1 = ([1], [1, 2, 1])
-	r1, t1 = impL1(sys1, n=10)
-	r2, t2 = np.array([[ -2.00000000e+00, -1.00000000e+00, 2.41009246e-16, 1.95887461e-17, 
+	r1 = impL1(sys1, n=10)
+	r2 = np.array([[0.00000000e+00, -1.00000000e+00, 2.41009246e-16, 1.95887461e-17, 
 		    -8.67469990e-33, -3.83718972e-34, 2.47373167e-49, 7.51657350e-51, 
-		    -6.36281335e-66, -1.47240249e-67]]), np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+		    -6.36281335e-66, -1.47240249e-67]])
 	assert np.allclose(r1, r2, atol=1e-8, rtol=1e-5)
 
 if __name__ == '__main__':
