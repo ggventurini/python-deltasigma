3fd07747b30d5fcfb77357659e22f414fb96a852
ggventurini <ggventurini+GITHUB@gmail.com>
2013-11-29 17:12:54 +0100

    Debugged clans(), added unit test.

--- pydelsigma/_clans.py
+++ pydelsigma/_clans.py
@@ -14,7 +14,6 @@
 # LICENSE file for the licensing terms.
 
 from __future__ import division
-from warnings import warn
 import numpy as np
 from scipy.optimize import minimize
 from scipy.signal import dimpulse
@@ -35,7 +34,6 @@ def clans(order=4, OSR=64, Q=5, rmax=0.95, opt=0):
     CLANS stands for Closed-Loop Analysis of Noise-Shapers,
     and it was originally developed by J.G. Kenney and L.R. Carley.
     """
-    warn("Warning untested function.")
     # Create the initial guess
     Hz, poles, _ = synthesizeNTF(order, OSR, opt, 1 + Q, 0)
     x = np.zeros((order, ))
@@ -62,7 +60,7 @@ def clans(order=4, OSR=64, Q=5, rmax=0.95, opt=0):
     #fobj = lambda x: dsclansObj6a(x, order, OSR, Q, rmax, Hz)
     #fconstr = lambda x: dsclansObj6b(x, order, OSR, Q, rmax, Hz)
     res = minimize(dsclansObja, x, args=(order, OSR, Q, rmax, Hz), 
-             method='COBYLA', constraints={'type':'ineq', 'fun':dsclansObjb, 
+             method='slsqp', constraints={'type':'ineq', 'fun':dsclansObjb, 
              'args':(order, OSR, Q, rmax, Hz)})
     NTF = dsclansNTF(res['x'], order, rmax, Hz)
     return NTF
@@ -84,3 +82,13 @@ def dsclansObjb(x, order, OSR, Q, rmax, Hz):
     # dimpulse(H, n=100)[y is 0][output 0]
     g = np.sum(np.abs(dimpulse(H, t=np.arange(100))[1][0])) - 1 - Q
     return -g
+
+def test_clans():
+    """Test unit for clans()"""
+    ntf = clans(5, 32, 5, .95, 1)
+    poles = np.array((0.41835234+0.j, 0.48922229+0.1709716j, 
+                      0.48922229-0.1709716j, 0.65244885+0.3817224j, 
+                      0.65244885-0.3817224j))
+    print "Poles:", ntf[1]
+    assert np.allclose(poles, ntf[1], atol=1e-8, rtol=1e-5)
+
