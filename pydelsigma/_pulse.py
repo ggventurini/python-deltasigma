4cc0a3253bee0a2fb0dcde6f71aa65fdffcd1aa9
ggventurini <ggventurini+GITHUB@gmail.com>
2013-12-02 17:35:36 +0100

    Bugfix. Now passes unit test (and other tests).

--- pydelsigma/_pulse.py
+++ pydelsigma/_pulse.py
@@ -19,7 +19,7 @@ pulse response of a CT system.
 
 from __future__ import division
 import numpy as np
-from scipy.signal import step, lti
+from scipy.signal import step2, lti
 
 from ._utils import lcm, rat
 
@@ -31,7 +31,7 @@ def pulse(S, tp=(0., 1.), dt=1., tfinal=10., nosum=False):
 	y	The pulse response
 	
 	Inputs
-	S	An LTI object specifying the system.
+	S	A **list** of LTI objects specifying the system.
 	tp	An (n, 2) array of pulse timings
 	dt	The time increment
 	tfinal	The time of the last desired sample
@@ -48,7 +48,7 @@ def pulse(S, tp=(0., 1.), dt=1., tfinal=10., nosum=False):
 
 	# Compute the time increment
 	dd = 1;
-	for tpi in np.nditer(np.array(tp)):
+	for tpi in np.nditer(tp.T.copy(order='C')):
 		x, di = rat(tpi, 1e-3)
 		dd = lcm(di, dd)
 
@@ -56,43 +56,57 @@ def pulse(S, tp=(0., 1.), dt=1., tfinal=10., nosum=False):
 	x, df = rat(tfinal, 1e-3)
 	delta_t = 1. / lcm(dd, lcm(ddt, df))
 	delta_t = max(1e-3, delta_t)	# Put a lower limit on delta_t
-	y1, T1 = step(S, T=np.arange(0., tfinal + delta_t, delta_t))
+	y1 = None
+	for Si in S:
+		T1, y1i= step2(Si, T=np.arange(0., tfinal + delta_t, delta_t))
+		if y1 is None:
+			y1 = y1i.reshape((y1i.shape[0], 1, 1))
+		else:
+			y1 = np.concatenate((y1, 
+                                             y1i.reshape((y1i.shape[0], 1, 1))), 
+                                            axis=2)
 
-	nd = np.round(dt/delta_t, 0)
-	nf = np.round(tfinal/delta_t, 0)
+	nd = int(np.round(dt/delta_t, 0))
+	nf = int(np.round(tfinal/delta_t, 0))
 	ndac = tp.shape[0]
-	if type(S) == tuple and type(S[0]) == tuple and np.isscalar(S[0][0]):
-		S = [[lti(S)]]
-	ni = len(S)
+	# This could be a way to check that we got a list of zpk/tf/lti objects 
+	# instead of a single object. Right now, we do not check. 
+	#if type(S) == tuple and type(S[0]) == tuple and np.isscalar(S[0][0]):
+	#	S = [[lti(S)]]
+	ni = len(S) # number of inputs
 	
-	if (ni % ndac) != 0:
+	if ni % ndac != 0:
 		raise ValueError, 'The number of inputs must be divisible by the number of dac timings.'
 		# Original comment from the MATLAB sources:
 		# This requirement comes from the complex case, where the number of inputs
 		# is 2 times the number of dac timings. I think this could be tidied up.
 
-	nis = ni/ndac # Number of inputs grouped together with a common DAC timing
-	              # (2 for the complex case)
+	nis = int(ni/ndac) # Number of inputs grouped together with a common DAC timing
+	                   # (2 for the complex case)
+	# Notice the number of outputs is hard-coded to one in the following
+	# delsigma actually allows for supplying pulse() objects that have 
+	# a variable number of outputs. As we have no MIMO description available,
+	# we consider only 1-output systems
 	if not nosum: # Sum the responses due to each input set
-		y = np.zeros((np.ceil(tfinal/float(dt)) + 1, len(S[0]), nis))
+		y = np.zeros((np.ceil(tfinal/float(dt)) + 1, 1, nis))
 	else:
-		y = np.zeros((np.ceil(tfinal/float(dt)) + 1, len(S[0]), ni))
+		y = np.zeros((np.ceil(tfinal/float(dt)) + 1, 1, ni))
 
 	if len(y1.shape) == 1:
 		y1 = y1.reshape((y1.shape[0], 1, 1))
 	#y1_columns = y1.shape[1] if len(y1.shape) > 1 else 1
 	for i in range(ndac):
-		n1 = np.round(tp[i, 0]/delta_t, 0)
-		n2 = np.round(tp[i, 1]/delta_t, 0)
+		n1 = int(np.round(tp[i, 0]/delta_t, 0))
+		n2 = int(np.round(tp[i, 1]/delta_t, 0))
 		z1 = (n1, y1.shape[1], nis)
 		z2 = (n2, y1.shape[1], nis)
-		yy = + np.concatenate((np.zeros(z1), y1[:nf-n1+1, :, (i-1)*nis:i*nis+1]), axis=0) \
-		     - np.concatenate((np.zeros(z2), y1[:nf-n2+1, :, (i-1)*nis:i*nis+1]), axis=0)
+		yy = + np.concatenate((np.zeros(z1), y1[:nf-n1+1, :, i*nis:(i + 1)*nis]), axis=0) \
+		     - np.concatenate((np.zeros(z2), y1[:nf-n2+1, :, i*nis:(i + 1)*nis]), axis=0)
 		yy = yy[::nd, :, :]
 		if not nosum: # Sum the responses due to each input set
 			y = y + yy
 		else:
-			y[:, :, i] = yy
+			y[:, :, i] = yy.reshape(yy.shape[0:2])
 	return y
 
 def test_pulse():
@@ -100,12 +114,10 @@ def test_pulse():
 	import pkg_resources
 	import scipy.io
 	H = ([1], [1, 2, 10])
-	pp = pulse(H, tp=(0., 1.), dt=.1, tfinal=10., nosum=False)
+	pp = pulse([H], tp=(0., 1.), dt=.1, tfinal=10., nosum=False)
 	pp = pp.reshape((pp.shape[0],1))
 	fname = pkg_resources.resource_filename(__name__, "test_data/test_pulse.mat")
 	pp2 = scipy.io.loadmat(fname)['pp']
 	assert np.allclose(pp, pp2, atol=1e-6, rtol=1e-4)
 	# FIXME ALSO CHECK MIMO TFS
-	
-if __name__ == '__main__':
-	test_pulse()
+
