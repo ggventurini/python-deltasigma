1537639c489081bed0b8a53a0a038326d8a007c1
ggventurini <ggventurini+GITHUB@gmail.com>
2013-12-11 18:21:40 +0100

    Many fixes for f0=0, improved docsctring and added unit test.

--- pydelsigma/_mapCtoD.py
+++ pydelsigma/_mapCtoD.py
@@ -21,54 +21,59 @@ import collections
 from warnings import warn
 
 import numpy as np
-from scipy.signal import lti, cont2discrete
+from numpy.random import rand
+from numpy.linalg import cond
+from scipy.linalg import inv, expm, norm
+from scipy.signal import lti, cont2discrete, ss2zpk
 
-from pydelsigma import padb, padr, partitionABCD
+from ._padb import padb
+from ._padr import padr
+from ._utils import _getABCD
+from ._constants import eps
 
 def mapCtoD(sys_c, t=(0, 1), f0=0.):
     """Map a MIMO continuous-time system to a SIMO discrete-time equivalent.
 
     The criterion for equivalence is that the sampled pulse response
     of the CT system must be identical to the impulse response of the DT system.
-    i.e. If yc is the output of the CT system with an input vc taken
-    from a set of DACs fed with a single DT input v, then y, the output
-    of the equivalent DT system with input v satisfies
-    y(n) = yc(n-) for integer n. The DACs are characterized by
+    i.e. If ``yc`` is the output of the CT system with an input ``vc`` taken
+    from a set of DACs fed with a single DT input ``v``, then ``y``, the output
+    of the equivalent DT system with input ``v`` satisfies:
+    ``y(n) = yc(n-)`` for integer ``n``. The DACs are characterized by
     rectangular impulse responses with edge times specified in the t list.
     
-    Input:
-    ======
+    **Input:**
 
-     arg1, 
-         the LTI description of the CT system, which can be:
-         * the ABCD matrix,
-         * a list-like containing the A, B, C, D matrices,
-         * a list of zpk tuples (internally converted to SS representation). 
+    arg1 : object 
+           the LTI description of the CT system, which can be:
+    * the ABCD matrix,
+    * a list-like containing the A, B, C, D matrices,
+    * a list of zpk tuples (internally converted to SS representation).
 
-     t	The edge times of the DAC pulse used to make CT waveforms 
-    	from DT inputs. Each row corresponds to one of the system
-        inputs; [-1 -1] denotes a CT input. The default is [0 1],
-        for all inputs except the first.
+    t : array_like
+        The edge times of the DAC pulse used to make CT waveforms 
+    	 from DT inputs. Each row corresponds to one of the system
+         inputs; [-1 -1] denotes a CT input. The default is [0 1],
+         for all inputs except the first.
 
-     f0	The (normalized) frequency at which the Gp filters' gains are
-    	to be set to unity. Default 0 (DC).
+    f0 : float
+         The (normalized) frequency at which the Gp filters' gains are
+         to be set to unity. Default 0 (DC).
     
-    Output:
-    =======
+    **Output:**
 
-     sys	The LTI description for the DT equivalent
+    sys : tuple
+         the LTI description for the DT equivalent, in A, B, C, D 
+         representation.
 
-     Gp	The mixed CT/DT prefilters which form the samples 
-    	fed to each state for the CT inputs.
+    Gp : list of lists
+         the mixed CT/DT prefilters which form the samples 
+         fed to each state for the CT inputs.
 
-    Reference:
-    ==========
-
-    R. Schreier and B. Zhang, "Delta-sigma modulators employing continuous-time
-    circuitry," IEEE Transactions on Circuits and Systems I, vol. 43, no. 4,
-    pp. 324-332, April 1996.
+    .. seealso:: R. Schreier and B. Zhang, "Delta-sigma modulators employing \
+    continuous-time circuitry," IEEE Transactions on Circuits and Systems I, \
+    vol. 43, no. 4, pp. 324-332, April 1996.
     """
-    warn("Untested, not working mapCtoD function.")
     # You need to have A, B, C, D specification of the system
     Ac, Bc, Cc, Dc = _getABCD(sys_c)
     ni = Bc.shape[1]
@@ -118,10 +123,10 @@ def mapCtoD(sys_c, t=(0, 1), f0=0.):
 
     # Add in the contributions from each DAC
     for i in np.flatnonzero(di):
-        t1=t[i, 0]
-        t2=t[i, 1]
-        B2=B[:, i]
-        D2=D[:, i]
+        t1 = t[i, 0]
+        t2 = t[i, 1]
+        B2 = B[:, i]
+        D2 = D[:, i]
         if t1 == 0 and t2 == 1 and D2 == 0: # No fancy stuff necessary
             Bp = Bp + padb(B2, npp)
         else:
@@ -151,174 +156,136 @@ def mapCtoD(sys_c, t=(0, 1), f0=0.):
                 Bp = Bp + padb(Btmp, npp)
             if n2 > 0:
                 Cp = Cp + padr(np.hstack((np.zeros((D2.shape[0], n + n1)), D2*np.ones((1, n2)))), npp)
-    sys = (Ap, Bp, Cp, Dp, 1)
+    sys = (Ap, Bp, Cp, Dp)
     if np.any(~di):
         # Compute the prefilters and add in the CT feed-ins.
         # Gp = inv(sI - Ac)*(zI - A)/z*Bc1
         n, m = Bc1.shape
-        Gp = _cell_like_list((n, m))
-        ztf = np.zeros(Bc1.shape) # !!Make this like stf: an array of zpk objects
+        Gp = np.empty_like(np.zeros((n, m)), dtype=object)
+        # !!Make this like stf: an array of zpk objects
+        ztf = np.empty_like(Bc1, dtype=object) 
         # Compute the z-domain portions of the filters
-        ABc1 = A*Bc1
-        for h in range(1,(m+1)):
-            for i in range(1,(n+1)):
-                if Bc1[(i-1),(h-1)] == 0:
-                    ztf[(i-1),(h-1)]=zpk(np.array([]),0,- ABc1[(i-1),(h-1)],1)
+        ABc1 = np.dot(A, Bc1)
+        for h in range(m):
+            for i in range(n):
+                if Bc1[i, h] == 0:
+                    ztf[i, h] = (np.array([]), 
+                                 np.array([0.]), 
+                                 -ABc1[i, h]) # dt=1
                 else:
-                    ztf[(i-1),(h-1)]=zpk(ABc1[(i-1),(h-1)] / Bc1[(i-1),(h-1)],0,Bc1[(i-1),(h-1)],1)
+                    ztf[i, h] = (np.atleast_1d(ABc1[i, h]/Bc1[i, h]),
+                                 np.array([0.]), 
+                                 Bc1[i, h]) # dt = 1
         # Compute the s-domain portions of each of the filters
-        stf=zpk(ss(Ac,eye(n),eye(n),np.zeros(n,n))) #Doesn't do pole-zero cancelation
-        for h in range(1,(m+1)):
-            for i in range(1,(n+1)):
-                k=1
-                for j in range(1,(n+1)):
-                    if stf.k(i,j) != 0 & ztf[(j-1)].k != 0: #A non-zero term
-                        Gp[(i-1),(h-1)](k).Hs=stf[(i-1),(j-1)]
-                        Gp[(i-1),(h-1)](k).Hz=ztf[(j-1),(h-1)]
-                        k=k + 1
+        stf = np.empty_like(np.zeros((n, n)), dtype=object) # stf[out, in] = zpk
+        for oi in range(n):
+            for ii in range(n):
+                # Doesn't do pole-zero cancellation
+                stf[oi, ii] = ss2zpk(Ac, np.eye(n), np.eye(n)[oi, :], 
+                                     np.zeros((1, n)), input=ii)
+                # scipy as of v 0.13 has no support for LTI MIMO systems
+                # only 'MISO', therefore you can't write:
+                # stf = ss2zpk(Ac, eye(n), eye(n), np.zeros(n, n))) 
+        for h in range(m):
+            for i in range(n):
+                # k = 1 unneded, see below
+                for j in range(n):
+                    # check the k values for a non-zero term
+                    if stf[i, j][2] != 0 and ztf[j, h][2] != 0: 
+                        if Gp[i, h] is None:
+                            Gp[i, h] = {}
+                            Gp[i, h].update({'Hs':[list(stf[i, j])]})
+                            Gp[i, h].update({'Hz':[list(ztf[j, h])]})
+                        else:
+                            Gp[i, h].update({'Hs':Gp[i, h]['Hs'] + [list(stf[i, j])]})
+                            Gp[i, h].update({'Hz':Gp[i, h]['Hz'] + [list(ztf[j, h])]})
+                        # the MATLAB-like cell code for the above statements would have
+                        # been: 
+                        #Gp[i, h](k).Hs = stf[i, j]
+                        #Gp[i, h](k).Hz = ztf[j, h]
+                        #k = k + 1
         if f0 != 0: # Need to correct the gain terms calculated by c2d
             # B1 = gains of Gp @f0;
-            for h in range(1,(m+1)):
-                for i in range(1,(n+1)):
-                    B1[(i-1),(h-1)]=evalMixedTF(Gp[(i-1),(h-1)],f0)
+            for h in range(m):
+                for i in range(n):
+                    B1[i, h] = evalMixedTF(Gp[i, h], f0)
                     # abs() used because ss() whines if B has complex entries...
                     # This is clearly incorrect.
                     # I've fudged the complex stuff by including a sign....
-                    B1[(i-1),(h-1)]=abs(B1[(i-1),(h-1)]) * sign(real(B1[(i-1),(h-1)]))
-                    if abs(B1[(i-1),(h-1)]) < 1e-09:
-                        B1[(i-1),(h-1)]=1e-09 # This prevents NaN in "line 174" below
+                    B1[i, h] = np.abs(B1[i, h]) * np.sign(np.real(B1[i, h]))
+                    if np.abs(B1[i, h]) < 1e-09:
+                        B1[i, h] = 1e-09 # This prevents NaN in "line 174" below
         # Adjust the gains of the pre-filters
-        for h in range(1,(m+1)):
-            for i in range(1,(n+1)):
-                for j in range(1,(max(Gp[(i-1),(h-1)].shape)+1)):
+        for h in range(m):
+            for i in range(n):
+                for j in range(max(len(Gp[i, h]['Hs']), len(Gp[i, h]['Hz']))):
                     # The next is "line 174"
-                    Gp[(i-1),(h-1)](j).Hs.k=Gp[(i-1),(h-1)](j).Hs.k / B1[(i-1),(h-1)]
-        set_(sys,'b',np.array([padb(B1,npp),sys.b]).reshape(1,-1),'d',np.array([D1,sys.d]).reshape(1,-1))
+                    Gp[i, h]['Hs'][j][2] = Gp[i, h]['Hs'][j][2]/B1[i, h]
+        sys = (sys[0], # Ap
+               np.hstack((padb(B1, npp), sys[1])), # new B
+               sys[2], # Cp
+               np.hstack((D1, sys[3]))) # new D
     return sys, Gp
 
 def _B2formula(Ac, t1, t2, B2):
-    if t1 == 0 & t2 == 0:
-        term=B2
+    warn("Untested, not working B2formula function.")
+    if t1 == 0 and t2 == 0:
+        term = B2
         return term
-    n=Ac.shape[0]
-    tmp=eye(n) - expm(- Ac)
+    n = Ac.shape[0]
+    tmp = np.eye(n) - expm(-Ac)
     if cond(tmp) < 1000000.0:
-        term=((expm(- Ac * t1) - expm(- Ac * t2)) * inv(tmp)) * B2
+        term = np.dot(((expm(-Ac*t1) - expm(-Ac*t2))*inv(tmp)), B2)
         return term
     # Numerical trouble. Perturb slightly and check the result
-    ntry=0
-    k=sqrt(eps)
-    Ac0=Ac
+    ntry = 0
+    k = np.sqrt(eps)
+    Ac0 = Ac
     while ntry < 2:
-
-        Ac=Ac0 + k * rand(n,n)
-        tmp=eye(n) - expm(- Ac)
-        if cond(tmp) < 1 / sqrt(eps):
-            ntry=ntry + 1
+        Ac = Ac0 + k*rand(n,n)
+        tmp = np.eye(n) - expm(-Ac)
+        if cond(tmp) < 1/np.sqrt(eps):
+            ntry = ntry + 1
             if ntry == 1:
-                term=((expm(- Ac * t1) - expm(- Ac * t2)) * inv(tmp)) * B2
+                term = np.dot(((expm(-Ac*t1) - expm(-Ac*t2))*inv(tmp)), B2)
             else:
-                term1=((expm(- Ac * t1) - expm(- Ac * t2)) * inv(tmp)) * B2
-        k=k * sqrt(2)
+                term1 = np.dot(((expm(-Ac*t1) - expm(-Ac*t2))*inv(tmp)), B2)
+        k = k*np.sqrt(2)
 
     if norm(term1 - term) > 0.001:
         warn('Warning: Inaccurate calculation in mapCtoD.')
     return term
 
-def _getABCD(arg):
-    """Utility method to convert the input arg to an A, B, C, D representation.
-
-    Parameters:
-
-    arg, which may be:
-
-    * ZPK tuple
-    * num, den tuple
-    * A, B, C, D tuple
-    * a sequence of the tuples of any of the above types.
-
-    Returns
-
-    The sequence of ndarrays A, B, C, D
-
-    Raises
-
-    TypeError
-    """
-    if isinstance(arg, np.ndarray):
-        # ABCD matrix
-        A, B, C, D = partitionABCD(arg)
-    elif _is_zpk(arg) or _is_num_den(arg) or _is_A_B_C_D(arg):
-       sys = lti(*arg) 
-       A, B, C, D = sys.A, sys.B, sys.C, sys.D
-    elif isinstance(arg, collections.Iterable):
-        A, B, C, D = None, None, None, None
-        for i in arg:
-            # Note we do not check if the user has assembled a list with 
-            # mismatched lti representations. 
-            sys = lti(*i) 
-            if A is None:
-                A = sys.A
-            elif not np.allclose(sys.A, A, atol=1e-8, rtol=1e-5):
-                raise ValueError("Mismatched lti list, A matrix disagreement.")
-            else:
-                pass
-            if B is None:
-                B = sys.B
-            else:
-                B = np.hstack((B, sys.B))
-            if C is None:
-                C = sys.C
-            elif not np.allclose(sys.C, C, atol=1e-8, rtol=1e-5):
-                raise ValueError("Mismatched lti list, C matrix disagreement.")
-            if D is None:
-                D = sys.D
-            else:
-                D = np.hstack((D, sys.D))
-    else:
-        raise TypeError("Unknown LTI representation: %s" % arg)
-    return A, B, C, D
-
-def _is_zpk(arg):
-    """Can the argument be safely assumed to be a zpk tuple?"""
-    return isinstance(arg, collections.Iterable) and len(arg) == 3 and \
-       isinstance(arg[0], collections.Iterable) and \
-       isinstance(arg[1], collections.Iterable) and np.isscalar(arg[2])
-
-def _is_num_den(arg):
-    """Can the argument be safely assumed to be a num, den tuple?"""
-    return isinstance(arg, collections.Iterable) and len(arg) == 2 and \
-           isinstance(arg[0], collections.Iterable) and \
-           isinstance(arg[1], collections.Iterable)
-
-def _is_A_B_C_D(arg):
-    """Can the argument be safely assumed to be an (A, B, C, D) tuple?"""
-    return isinstance(arg, collections.Iterable) and len(arg) == 4 and \
-           isinstance(arg[0], collections.Iterable) and \
-           isinstance(arg[1], collections.Iterable) and \
-           isinstance(arg[2], collections.Iterable) and \
-           isinstance(arg[3], collections.Iterable)
-
-def _cell_like_list(shape, init=None):
-    """Returns a list of lists (possibly of lists... etc...), 
-    with all values initialized to `init`.
-
-    Parameters
-
-    shape: tuple of ints, 
-           the dimensions of the desidered object
-
-    init: object, optional,
-          the initialization value for every element in the object
-
-    Returns
-    
-    cell, a list of lists (...)
-    """
-    a = []
-    for i in range(shape[0]):
-        if len(shape) == 1:
-            a.append(init)
-        else:
-            a.append(_cell_like_list(shape[1:], init))
-    return a
+def test_mapCtoD():
+    """Unit test for mapCtoD"""
+    from ._calculateTF import calculateTF
+    # The first test comes straight from the DSToolbox doc
+    # We map the standard continuous time DS modulator to its DT counterpart
+    # and we check whether the TF is (1 - 1/z)**2
+    LFc = (np.array([[0., 0.], [1., 0.]]), 
+           np.array([[1., -1.], [0., -1.5]]), 
+           np.array([[0., 1.]]), 
+           np.array(([[0., 0.]]))
+          )
+    tdac = [0, 1]
+    LF, Gp = mapCtoD(LFc, tdac)
+    ABCD = np.vstack((np.hstack((LF[0], LF[1])),
+                      np.hstack((LF[2], LF[3]))
+                    ))
+    H = calculateTF(ABCD)
+    assert np.allclose(H[0].num, [ 1., -2.,  1.], atol=1e-8, rtol=1e-5)
+    assert np.allclose(H[0].den, [1., 0., 0.], atol=1e-8, rtol=1e-5)
+    ABCDc = np.array([[0., 0.,  0., 0.04440879, -0.04440879],
+                      [1., 0., -0.00578297, 0., -0.23997611],
+                      [0., 1.,  0., 0., -0.67004646],
+                      [0., 0.,  1., 0.,  0.]])
+    tdac = np.array([[-1., -1], [0., 1.]])
+    sys_d, Gp = mapCtoD(ABCDc, tdac)
+    ABCD = np.vstack((np.hstack((sys_d[0], sys_d[1])),
+                      np.hstack((sys_d[2], sys_d[3]))
+                    ))
+    ABCDref = np.array([[1., 0., 0., 0.04440879, -0.04440879],
+                        [0.99903645, 0.99710991, -0.0057774, 0.0221937, -0.26000208],
+                        [0.49975909, 0.99903645,  0.99710991, 0.00739932, -0.79673041],
+                        [0., 0., 1., 0., 0.]])
+    assert np.allclose(ABCD, ABCDref, atol=1e-8, rtol=1e-5)
