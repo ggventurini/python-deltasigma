b26fcf89c92fec4e7b142860c1c670005977eb38
ggventurini <ggventurini+GITHUB@gmail.com>
2013-12-11 17:54:41 +0100

    Improved docstring.

--- pydelsigma/_lollipop.py
+++ pydelsigma/_lollipop.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # _lollipop.py
 # The lollipop function: plot digital samples like it was a lollipop field.
-# that do not find a direct replacement in numpy/scipy.
 # Copyright 2013 Giuseppe Venturini
 # This file is part of python-deltasigma.
 #
@@ -14,24 +13,55 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # LICENSE file for the licensing terms.
 
-"""Lollipop plotting functions.
+"""Lollipop plotting function.
 """
 
 import numpy as np
 import pylab as plt
 from numpy.matlib import repmat
 
+# Plot lollipops (o's and sticks)
+# 
+#	   ^
+#	   | o     o     o         o o o   o o 
+#	   | |   o | o o | o     o | | | o | | 
+#	   | | o | | | | | | o o | | | | | | | 
+#          +----------------------------------->
+
 def lollipop(x, y, color='b', lw=2, ybot=0):
 	"""Plot lollipops (o's and sticks)
 	
-	o     o     o         o o o   o o
-	|   o | o o | o     o | | | o | |
-	| o | | | | | | o o | | | | | | |
-	
-	Returns:
-	========
+	**Parameters:**
+
+	x, y : ndarray
+	       data to be plotted
+
+	color : any matplotlib color
+	        plotting color
+
+	lw : float
+             line width value in points
+
+	ybot : float
+               ground level for the sticks.
+
+	**Returns:**
+
 	None
 	
+	**Example:**
+
+	.. plot::
+
+	    import matplotlib.pyplot as plt
+	    import numpy as np
+	    from pydelsigma import lollipop
+	    t = np.arange(1, 20)*1e-3
+	    f = 20.
+	    a = np.sin(2*np.pi*f*t)
+	    lollipop(t, a)
+	    plt.grid(True)
+	    plt.show()
 	"""
 
 	hold_status = plt.ishold()
@@ -39,7 +69,7 @@ def lollipop(x, y, color='b', lw=2, ybot=0):
 	lolli_fmt = {'linewidth': lw, 'color': color}
 	pop_fmt = {'markeredgecolor': color, 'markerfacecolor':'None', 'linestyle': 'None', \
 	                'marker': 'o', 'markersize':10, 'markeredgewidth': lw*1.1}
-	# Plot circles
+	# Plot the circles
 	plt.plot(x, y, **pop_fmt)
 
 	# Make x and y row vectors, then plot as sticks
@@ -52,6 +82,9 @@ def lollipop(x, y, color='b', lw=2, ybot=0):
 	plt.hold(hold_status)
 
 if __name__ == '__main__':
-	t = np.arange(10)
-	lollipop(t, t)
+	t = np.arange(1, 20)*1e-3
+	f = 20.
+	a = np.sin(2*np.pi*f*t)
+	lollipop(t, a)
+	plt.grid(True)
 	plt.show()
