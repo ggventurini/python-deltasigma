ce88945fdf88f746aad336bca6d3c40cf8cfd8d9
ggventurini <ggventurini+GITHUB@gmail.com>
2013-12-11 17:25:41 +0100

    Fix docstring, remove __main__, add support for scalars.

--- pydelsigma/_SIunits.py
+++ pydelsigma/_SIunits.py
@@ -14,19 +14,8 @@
 # LICENSE file for the licensing terms.
 
 """This module provides the SIunits() function, which, given an input,
-returns factor (think "engineering notation") and a SPICE-like alphabetic
+returns factor (think "engineering notation") and an alphabetic
 suffix.
-
-The factors and suffixes supported are:
-1e-3	m	milli		1e3	k	kilo
-1e-6	u	micro		1e6	M	mega
-1e-9	n	nano		1e9	G	giga
-1e-12	p	pico		1e12	T	tera
-1e-15	f	femto		1e15	P	peta
-1e-18	a	atto		1e18	E	exa			
-1e-21	z	zepto		1e21	Z	zeta
-1e-24 y	yocto		1e24	Y	yotta
-
 """
 
 from __future__ import division
@@ -34,17 +23,50 @@ import numpy as np
 from ._constants import eps
 
 def SIunits(x):
-	""" The factors and suffixes supported are:
- 1e-3	m	milli		1e3	k	kilo
- 1e-6	u	micro		1e6	M	mega
- 1e-9	n	nano		1e9	G	giga
- 1e-12	p	pico		1e12	T	tera
- 1e-15	f	femto		1e15	P	peta
- 1e-18	a	atto		1e18	E	exa			
- 1e-21	z	zepto		1e21	Z	zeta
- 1e-24 y	yocto		1e24	Y	yotta"""
+	"""Calculates the factor for representing the float `x` in engineering 
+	notation.
+
+	The factors and suffixes supported are::
+
+	    1e-3	m	milli		1e3	k	kilo
+	    1e-6	u	micro		1e6	M	mega
+	    1e-9	n	nano		1e9	G	giga
+	    1e-12	p	pico		1e12	T	tera
+	    1e-15	f	femto		1e15	P	peta
+	    1e-18	a	atto		1e18	E	exa			
+	    1e-21	z	zepto		1e21	Z	zeta
+	    1e-24	y	yocto		1e24	Y	yotta
+
+	**Parameters:**
+
+	x : scalar or array_like
+            The number for which the engineering notation factor and suffix
+	    are to be calculated.
+
+	**Returns:**
+
+	factor : float or list of floats
+	         the engineering notation factor(s)
+
+	prefix : string or list of strings
+	         the engineering notation unit prefix(es)
+
+	**Example**::
+
+	    a = 3300.
+	    unit = 'g'
+	    f, p = SIunits(a)
+	    print "Float 'a' in engineering notation: %.3f %s%s" % (a/f, p, unit)
+
+	Prints::
+
+	    Float 'a' in engineering notation: 3.300 kg
+	"""
 	prefixes_n = ('', 'm', 'u', 'n', 'p', 'f', 'a', 'z', 'y')
 	prefixes_p = ('', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+	scalar_input = np.isscalar(x)
+	if scalar_input:
+		x = [x]
 	factor = np.ones(len(x))
 	prefix = ['']*len(x)
 	for i in range(len(x)):
@@ -63,6 +85,8 @@ def SIunits(x):
 		#else:
 		#	prefix[i] = ''
 		#	factor[i] = 1
+	if scalar_input:
+		factor, prefix = factor[0], prefix[0]
 	return factor, prefix
 
 def test():
@@ -73,7 +97,9 @@ def test():
 	res = zip(f,p)
 	for r, c in zip(res, correct):
 		assert r[0] == c[0] and r[1] == c[1]
-	print "Test passed."
+	# test scalars
+	tv = 2100312.24
+	correct = (1e6, 'M')
+	f, p = SIunits(tv)
+	assert f == correct[0] and p == correct[1]
 
-if __name__ == '__main__':
-	test()
