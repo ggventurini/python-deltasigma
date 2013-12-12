7c25d3050bc024108fc561c3d80f02d67b67c832
ggventurini <ggventurini+GITHUB@gmail.com>
2013-12-12 19:23:07 +0100

    Use db() from _db.

--- pydelsigma/_ds_synNTFobj1.py
+++ pydelsigma/_ds_synNTFobj1.py
@@ -20,8 +20,9 @@ import numpy as np
 
 from ._padt import padt
 from ._padb import padb 
+from ._db import db
 from ._ds_f1f2 import ds_f1f2 
-from ._utils import zpk, db, carray
+from ._utils import zpk, carray
 from ._rmsGain import rmsGain
 
 def ds_synNTFobj1(x, p, osr, f0):
