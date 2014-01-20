595d84eab5bacc87e6b2f3e67ae88703b2b171d3
ggventurini <ggventurini+GITHUB@gmail.com>
2014-01-20 11:04:17 +0100

    Add unit test

--- deltasigma/_synthesizeNTF.py
+++ deltasigma/_synthesizeNTF.py
@@ -143,3 +143,46 @@ def synthesizeNTF(order=3, osr=64, opt=0, H_inf=1.5, f0=0.0):
     else:
         ntf = synthesizeNTF1(order, osr, opt, H_inf, f0)
     return ntf
+
+def test_synthesizeNTF():
+    """Test function for synthesizeNTF()"""
+    from ._utils import cplxpair
+    from ._config import optimize_NTF
+    optimize_NTF = True
+    # synthesizeNTF should have as default values:
+    # order=3, osr=64, opt=0, H_inf=1.5, f0=0.0
+    z, p, k = synthesizeNTF()
+    zref = [1., 1., 1.]
+    pref = [.6694, .7654 + .2793j, .7654 - .2793j]
+    kref = 1.
+    assert np.allclose(cplxpair(z), cplxpair(zref), atol=1e-4, rtol=1e-4)
+    assert np.allclose(cplxpair(p), cplxpair(pref), atol=1e-4, rtol=1e-4)
+    assert np.allclose(k, kref, atol=1e-4, rtol=1e-4)
+    # Up next: even order bandpass test
+    z, p, k = synthesizeNTF(order=4, osr=32, opt=0, H_inf=1.3, f0=.33)
+    zref = [-0.4818 + 0.8763j, -0.4818 - 0.8763j, -0.4818 + 0.8763j,
+            -0.4818 - 0.8763j]
+    pref = [-0.5125 - 0.7018j, -0.5125 + 0.7018j, -0.3233 - 0.8240j, 
+            -0.3233 + 0.8240j]
+    kref = 1.
+    assert np.allclose(cplxpair(z), cplxpair(zref), atol=1e-4, rtol=1e-4)
+    assert np.allclose(cplxpair(p), cplxpair(pref), atol=1e-4, rtol=1e-4)
+    assert np.allclose(k, kref, atol=1e-4, rtol=1e-4)
+    # repeat with zeros optimization
+    z, p, k = synthesizeNTF(opt=1)
+    zref = [1.0000 + 0.0000j, 0.9993 + 0.0380j, 0.9993 - 0.0380j]
+    pref = [0.7652 - 0.2795j, 0.7652 + 0.2795j, 0.6692 + 0.0000j]
+    kref = 1.
+    assert np.allclose(cplxpair(z), cplxpair(zref), atol=1e-4, rtol=1e-4)
+    assert np.allclose(cplxpair(p), cplxpair(pref), atol=1e-4, rtol=1e-4)
+    assert np.allclose(k, kref, atol=1e-4, rtol=1e-4)
+    # Up next: even order bandpass test
+    z, p, k = synthesizeNTF(order=4, osr=32, opt=1, H_inf=1.3, f0=.33)
+    zref = [-0.4567 + 0.8896j, -0.4567 - 0.8896j, -0.5064 + 0.8623j,
+            -0.5064 - 0.8623j]
+    pref = [-0.5125 - 0.7014j, -0.5125 + 0.7014j, -0.3230 - 0.8239j,
+            -0.3230 + 0.8239j]
+    kref = 1.
+    assert np.allclose(cplxpair(z), cplxpair(zref), atol=1e-4, rtol=1e-4)
+    assert np.allclose(cplxpair(p), cplxpair(pref), atol=1e-4, rtol=1e-4)
+    assert np.allclose(k, kref, atol=1e-4, rtol=1e-4)
