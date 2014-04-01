d4f2dc9aff7098c1ec249d5409a7fd7d468dc32f
ggventurini <ggventurini+GITHUB@gmail.com>
2014-04-01 12:54:23 +0200

    Fix docstring.

--- deltasigma/_rmsGain.py
+++ deltasigma/_rmsGain.py
@@ -21,8 +21,29 @@ from scipy.linalg import norm
 from ._evalTF import evalTF
 
 def rmsGain(H, f1, f2, N=100):
-	"""Compute the root mean-square gain of the discrete-time
-	TF ``H`` in the frequency band ``(f1, f2)``.
+	"""Compute the root mean-square gain of a discrete-time TF.
+
+	The computation is carried out over the frequency band ``(f1, f2)``,
+	employing ``N`` discretization steps.
+
+	**Parameters:**
+
+	H : object
+	    The discrete-time transfer function. See  :func:`evalTF` for the supported types.
+
+	f1 : scalar
+	    The start value in Hertz of the frequency band over which the gain is evaluated.
+
+	f2 : scalar
+	    The end value (inclusive) in Hertz of the aforementioned frequency band.
+
+	N : integer, optional
+	    The number of discretization points to be taken over specified interval.
+
+	**Returns:**
+
+	Grms : scalar
+	    The root mean-square gain
 	"""
 
 	w = np.linspace(2*np.pi*f1, 2*np.pi*f2, N)
