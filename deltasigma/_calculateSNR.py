eda8ad037b3f84972faf1ee05e8537568adbedf4
ggventurini <ggventurini+GITHUB@gmail.com>
2014-06-17 17:54:38 +0200

    PEP8: fix spacing, indent.

--- deltasigma/_calculateSNR.py
+++ deltasigma/_calculateSNR.py
@@ -23,68 +23,68 @@ from numpy.linalg import norm
 from ._dbv import dbv
 
 def calculateSNR(hwfft, f, nsig=1):
-	"""Estimate the SNR from the FFT.
+    """Estimate the SNR from the FFT.
 
-	Estimate the Signal-to-Noise Ratio (SNR), given the in-band bins of
-	a Hann-windowed FFT and the location ``f0`` of the input signal (f>0).
-	For ``nsig = 1``, the input tone is contained in ``hwfft(f:f+2)``,
-	this range is appropriate for a Hann-windowed FFT.
+    Estimate the Signal-to-Noise Ratio (SNR), given the in-band bins of
+    a Hann-windowed FFT and the location ``f0`` of the input signal (f>0).
+    For ``nsig = 1``, the input tone is contained in ``hwfft(f:f+2)``,
+    this range is appropriate for a Hann-windowed FFT.
 
-	Each increment in ``nsig`` adds a bin to either side.
+    Each increment in ``nsig`` adds a bin to either side.
 
-	The SNR is expressed in dB.
+    The SNR is expressed in dB.
 
-	**Parameters:**
+    **Parameters:**
 
-	hwfft : sequence
-	        the FFT
+    hwfft : sequence
+            the FFT
 
-	f : integer
-	    Location of the input signal. Normalized.
+    f : integer
+        Location of the input signal. Normalized.
 
-	.. note:: f = 0 corresponds to DC, as Python indexing starts from 0.
+    .. note:: f = 0 corresponds to DC, as Python indexing starts from 0.
 
-	nsig : integer, optional
-	       Extra bins added to either side. Defaults to 1.
+    nsig : integer, optional
+           Extra bins added to either side. Defaults to 1.
 
-	**Returns:**
+    **Returns:**
 
-	SNR : scalar
-	      The computed SNR value in dB.
+    SNR : scalar
+          The computed SNR value in dB.
 
-	"""
-	hwfft = hwfft.squeeze()
-	signalBins = np.arange(f - nsig + 1, f + nsig + 2, dtype='int64')
-	signalBins = signalBins[signalBins > 0]
-	signalBins = signalBins[signalBins <= max(hwfft.shape)]
-	s = norm(hwfft[signalBins - 1]) # *4/(N*sqrt(3)) for true rms value;
-	noiseBins = np.arange(1, max(hwfft.shape) + 1, dtype='int64')
-	noiseBins = np.delete(noiseBins, noiseBins[signalBins - 1] - 1)
-	n = norm(hwfft[noiseBins - 1])
-	if n == 0:
-		snr = np.Inf
-	else:
-		snr = dbv(s/n)
-	return snr
+    """
+    hwfft = hwfft.squeeze()
+    signalBins = np.arange(f - nsig + 1, f + nsig + 2, dtype='int64')
+    signalBins = signalBins[signalBins > 0]
+    signalBins = signalBins[signalBins <= max(hwfft.shape)]
+    s = norm(hwfft[signalBins - 1]) # *4/(N*sqrt(3)) for true rms value;
+    noiseBins = np.arange(1, max(hwfft.shape) + 1, dtype='int64')
+    noiseBins = np.delete(noiseBins, noiseBins[signalBins - 1] - 1)
+    n = norm(hwfft[noiseBins - 1])
+    if n == 0:
+        snr = np.Inf
+    else:
+        snr = dbv(s/n)
+    return snr
 
 def test_calculateSNR():
-	"""Test function for calculateSNR()
-	"""
-	from numpy.fft import fft
-	from ._ds_hann import ds_hann
-	N = 2**12
-	t = np.arange(N)
-	f1, f2 = 1./8, 1./302
-	A = np.cos(2*np.pi*f1*t)
-	B = .01*np.cos(2*np.pi*f2*t)
-	y = A + B
-	window = ds_hann(N)
-	hwfft = fft(window*y)
-	snr = calculateSNR(hwfft[:N/2], int(N*f1))
-	assert np.allclose(snr, 40, atol=1e-8, rtol=1e-8)
-	hwfft = np.zeros((N/2, ))
-	hwfft[512] = 1. # specially crafted to have Inf snr
-	snr = calculateSNR(hwfft[:N/2], 512)
-	print(snr)
-	assert snr == np.Inf
+    """Test function for calculateSNR()
+    """
+    from numpy.fft import fft
+    from ._ds_hann import ds_hann
+    N = 2**12
+    t = np.arange(N)
+    f1, f2 = 1./8, 1./302
+    A = np.cos(2*np.pi*f1*t)
+    B = .01*np.cos(2*np.pi*f2*t)
+    y = A + B
+    window = ds_hann(N)
+    hwfft = fft(window*y)
+    snr = calculateSNR(hwfft[:N/2], int(N*f1))
+    assert np.allclose(snr, 40, atol=1e-8, rtol=1e-8)
+    hwfft = np.zeros((N/2, ))
+    hwfft[512] = 1. # specially crafted to have Inf snr
+    snr = calculateSNR(hwfft[:N/2], 512)
+    print(snr)
+    assert snr == np.Inf
 
--- deltasigma/_sinc_decimate.py
+++ deltasigma/_sinc_decimate.py
@@ -21,19 +21,19 @@ from __future__ import division
 import numpy as np
 
 def sinc_decimate(x, m, r):
-	"""Decimate ``x`` by an ``m``-th order sinc filter of length ``r``.
-	"""
-	x = x[:]
-	for i in range(m):
-		x = np.cumsum(x)
-		x = np.concatenate((x[:r], x[r:] - x[:-r]), axis=0)/r
-	return x[r::r]
+    """Decimate ``x`` by an ``m``-th order sinc filter of length ``r``.
+    """
+    x = x[:]
+    for _ in range(m):
+        x = np.cumsum(x)
+        x = np.concatenate((x[:r], x[r:] - x[:-r]), axis=0)/r
+    return x[r::r]
 
 def test_sinc_decimate():
-	"""Test function for sinc_decimate()"""
-	tv = np.sin(2*np.pi*.1*np.arange(0, 100))
-	r = [-1.33907057e-17, -3.55951662e-17, -2.28847549e-18, 
-	     -3.55951662e-17, -1.02208548e-16, -4.35275455e-16, 
-	      4.97311886e-16, -4.57479916e-16, 4.30698504e-16]
-	assert np.allclose(sinc_decimate(tv, 1, 10), r, rtol=1e-5, atol=1e-8)
+    """Test function for sinc_decimate()"""
+    tv = np.sin(2*np.pi*.1*np.arange(0, 100))
+    r = [-1.33907057e-17, -3.55951662e-17, -2.28847549e-18,
+         -3.55951662e-17, -1.02208548e-16, -4.35275455e-16,
+         4.97311886e-16, -4.57479916e-16, 4.30698504e-16]
+    assert np.allclose(sinc_decimate(tv, 1, 10), r, rtol=1e-5, atol=1e-8)
 
