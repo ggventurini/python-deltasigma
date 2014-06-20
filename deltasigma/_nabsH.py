5baf234e1834af0c9e16029adc8ccb8507ef75f2
ggventurini <ggventurini+GITHUB@gmail.com>
2014-06-20 11:19:02 +0200

    PEP8: fix trailing whitespace.

--- deltasigma/_nabsH.py
+++ deltasigma/_nabsH.py
@@ -4,7 +4,7 @@
 # Copyright 2013 Giuseppe Venturini
 # This file is part of python-deltasigma.
 #
-# python-deltasigma is a 1:1 Python replacement of Richard Schreier's 
+# python-deltasigma is a 1:1 Python replacement of Richard Schreier's
 # MATLAB delta sigma toolbox (aka "delsigma"), upon which it is heavily based.
 # The delta sigma toolbox is (c) 2009, Richard Schreier.
 #
@@ -13,7 +13,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # LICENSE file for the licensing terms.
 
-"""This module provides the nabsH() function, which computes the negative of 
+"""This module provides the nabsH() function, which computes the negative of
 the absolute value of H(z).
 """
 
