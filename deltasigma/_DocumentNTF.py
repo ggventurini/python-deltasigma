1f344bf65acf254bef352a4d82918bd8bdf07cfc
ggventurini <ggventurini+GITHUB@gmail.com>
2014-07-08 21:51:37 +0200

    MINOR: plot label, refactor, add units.

--- deltasigma/_DocumentNTF.py
+++ deltasigma/_DocumentNTF.py
@@ -111,8 +111,8 @@ def DocumentNTF(arg1, osr=64, f0=0, quadrature=False):
     plt.grid(True)
     y_bot = min(-80, np.round(NG0*1.1, -1))
     figureMagic(xRange=(f_left, 0.5), dx=1/20., yRange=(y_bot, 15), dy=10)
-    plt.ylabel('|H(f)|')
-    plt.xlabel('frequency')
+    plt.ylabel('|H(f)| dB')
+    plt.xlabel('Frequency ($1 \\rightarrow f_{s}$)')
     plt.title('Frequency Response')
     return
 
