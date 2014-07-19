a5ec8d4b5ad9e4a88eb40e68ce2d4d6bc5f96cd0
ggventurini <ggventurini+GITHUB@gmail.com>
2014-07-19 15:23:50 +0200

    TEST: Add test_template.py contributed by Shayne Hodge.

---
+++ deltasigma/tests/test_template.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+import deltasigma as ds
+
+class TestTestTemplate(unittest.TestCase):
+    """Class doc string"""
+    def setUp(self):
+        pass
+
+    def test_true_is_true(self):
+        """Function doc string"""
+        self.assertTrue(True)
