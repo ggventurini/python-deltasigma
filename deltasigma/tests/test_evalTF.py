878d7357fc7202cabb6e13aa3ecbcddd80779548
ggventurini <ggventurini+GITHUB@gmail.com>
2014-07-30 18:02:28 +0200

    TEST: more tests, docstring fixes.

--- deltasigma/tests/test_evalTF.py
+++ deltasigma/tests/test_evalTF.py
@@ -16,9 +16,11 @@
 import unittest
 import numpy as np
 import deltasigma as ds
+
 from scipy.signal import lti, tf2zpk
 from deltasigma._utils import empty
 
+from nose.tools import raises
 
 class TestEvalTF(unittest.TestCase):
     """Test functions for evalTF()"""
@@ -36,16 +38,34 @@ class TestEvalTF(unittest.TestCase):
         self.h2 = ds.evalTF(tstr2, z)
         self.h3 = ds.evalTF(H, z)
         self.h4 = ds.evalTF(lti(tstr2.zeros, tstr2.poles, tstr2.gain), z)
+        h5tf = lti(tstr2.zeros, tstr2.poles, tstr2.gain)
+        self.h5 = ds.evalTF((h5tf.A, h5tf.B, h5tf.C, h5tf.D), z)
+        h6tf = np.vstack((np.hstack((h5tf.A, h5tf.B)),
+                          np.hstack((h5tf.C, np.atleast_2d(h5tf.D)))))
+        self.h6 = ds.evalTF(h6tf, z)
 
     def test_evalTF_first(self):
-        """test_evalRPoly mag h1 == mag h2"""
+        """Test evalTF num/den obj form"""
         self.assertTrue(np.allclose(
             np.abs(self.h1), np.abs(self.h2), atol=1e-8, rtol=1e-5))
 
     def test_evalTF_second(self):
-        """test_evalRPoly h1 == h3"""
+        """Test evalTF zp obj form"""
         self.assertTrue(np.allclose(self.h1, self.h3, atol=1e-8, rtol=1e-5))
 
     def test_evalTF_third(self):
-        """test_evalRPoly h3 == h4"""
+        """Test evalTF zpk and num/den tuple form"""
         self.assertTrue(np.allclose(self.h3, self.h4, atol=1e-8, rtol=1e-5))
+
+    def test_evalTF_fourth(self):
+        """Test evalTF ABCD tuple form"""
+        self.assertTrue(np.allclose(self.h5, self.h4, atol=1e-8, rtol=1e-5))
+
+    def test_evalTF_fifth(self):
+        """Test evalTF ABCD matrix form"""
+        self.assertTrue(np.allclose(self.h6, self.h5, atol=1e-8, rtol=1e-5))
+
+    @raises(TypeError)
+    def test_evalTF_sixth(self):
+        """Test unknown forms error"""
+        ds.evalTF(empty(), [1, 2, 3])
