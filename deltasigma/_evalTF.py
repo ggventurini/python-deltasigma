c55481d8bfd6ff82da100adedd1d04825324cd3c
ggventurini <ggventurini+GITHUB@gmail.com>
2014-07-30 09:37:14 +0200

    TEST: remove tests from _evalRPoly.py and _evalTF.py

--- deltasigma/_evalTF.py
+++ deltasigma/_evalTF.py
@@ -75,24 +75,3 @@ def evalTF(tf, z):
         h = k*evalRPoly(zeros, z)/evalRPoly(poles, z)
     return h
 
-def test_evalTF():
-    """Test function for evalTF()
-    """
-    from scipy.signal import lti, tf2zpk
-    from ._utils import empty
-    num, den = np.poly([3, 0.3, 1]), np.poly([2, 0.5, .25])
-    H = (num, den)
-    tstr1 = empty()
-    tstr1.form, tstr1.num, tstr1.den = 'coeff', num, den
-    tstr2 = empty()
-    tstr2.form = 'zp'
-    tstr2.zeros, tstr2.poles, tstr2.gain = tf2zpk(num, den)
-    z = np.exp(1j*np.linspace(0, 2*np.pi, num=129, endpoint=True))
-    h1 = evalTF(tstr1, z)
-    h2 = evalTF(tstr2, z)
-    h3 = evalTF(H, z)
-    h4 = evalTF(lti(tstr2.zeros, tstr2.poles, tstr2.gain), z)
-    assert np.allclose(np.abs(h1), np.abs(h2), atol=1e-8, rtol=1e-5)
-    assert np.allclose(h1, h3, atol=1e-8, rtol=1e-5)
-    assert np.allclose(h3, h4, atol=1e-8, rtol=1e-5)
-
