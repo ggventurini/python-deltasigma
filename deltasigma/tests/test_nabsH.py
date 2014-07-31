4229aea78f528573440e4f02182ce41ee28f4981
ggventurini <ggventurini+GITHUB@gmail.com>
2014-07-31 15:23:39 +0200

    TEST: fix spacing 3.

--- deltasigma/tests/test_nabsH.py
+++ deltasigma/tests/test_nabsH.py
@@ -30,11 +30,11 @@ class TestNabsH(unittest.TestCase):
 
     def test_nabsH(self):
         """Test function for nabsH()"""
-	H = ([1, 2], [2, 0, .25], 1)
-	N = 129
-	w = np.linspace(0, 2*np.pi, num=N, endpoint=True)
-	z = np.exp(1j*w)
-	r1 = -np.abs(ds.evalTF(H, z))
-	r2 = ds.nabsH(w, H)
-	self.assertTrue(np.allclose(r1, r2, atol=1e-8, rtol=1e-5))
+        H = ([1, 2], [2, 0, .25], 1)
+        N = 129
+        w = np.linspace(0, 2*np.pi, num=N, endpoint=True)
+        z = np.exp(1j*w)
+        r1 = -np.abs(ds.evalTF(H, z))
+        r2 = ds.nabsH(w, H)
+        self.assertTrue(np.allclose(r1, r2, atol=1e-8, rtol=1e-5))
 
