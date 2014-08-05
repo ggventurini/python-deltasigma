c1c8aa8825be220093d9a4fa92dee2127babf506
ggventurini <ggventurini+GITHUB@gmail.com>
2014-08-05 09:38:05 +0200

    TEST: test template in misc/

--- misc/test_template.py
+++ misc/test_template.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# <filename>
-# This module provides the tests for the ____________ function.
-# Copyright 2014 Giuseppe Venturini & Shayne Hodge
+# test_CHANGEME.py
+# This module provides the tests for the CHANGEME function.
+# Copyright 2014 Giuseppe Venturini
 # This file is part of python-deltasigma.
 #
-# python-deltasigma is a 1:1 Python replacement of Richard Schreier's
+# python-deltasigma is a 1:1 Python replacement of Richard Schreier's 
 # MATLAB delta sigma toolbox (aka "delsigma"), upon which it is heavily based.
 # The delta sigma toolbox is (c) 2009, Richard Schreier.
 #
@@ -13,15 +13,20 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # LICENSE file for the licensing terms.
 
+"""This module provides the test class for the CHANGEME() function.
+"""
+
 import unittest
 import numpy as np
 import deltasigma as ds
 
-class TestTestTemplate(unittest.TestCase):
-    """Class doc string"""
+class TestCHANGEME(unittest.TestCase):
+    """Test class for CHANGEME()"""
+
     def setUp(self):
         pass
 
-    def test_true_is_true(self):
-        """Function doc string"""
-        self.assertTrue(True)
+    def test_CHANGEME(self):
+        """Test function for CHANGEME() 1/3"""
+        self.assertTrue()
+
