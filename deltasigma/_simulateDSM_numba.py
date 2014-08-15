1629db0856c111bd8fda1eb41e31fedbee85433d
Shayne Hodge <schodge@ieee.org>
2014-08-15 12:58:19 -0700

    Latest (failed) attempts to speed up.  Only using ABCD form.

--- deltasigma/_simulateDSM_numba.py
+++ deltasigma/_simulateDSM_numba.py
@@ -22,9 +22,7 @@ from warnings import warn
 
 import numpy as np
 
-from scipy.signal import tf2zpk, zpk2ss
-from scipy.linalg import orth, norm, inv
-from _utils import carray, _get_zpk
+from _utils import carray
 
 from numba import double, int32, complex64
 from numba.types import pyobject
@@ -37,66 +35,47 @@ from numba.decorators import jit, autojit
 # nlev = 1D int array
 # x0 = 1D double array
 
-#@jit(arg_types=[double[:, :], complex64[:], int32[:], double[:]])
-@autojit()
+# Temporarily rewriting to work only the ABCD form
+
 def simulateDSM(u, arg2, nlev=2, x0=0):
+    if isinstance(arg2, np.ndarray):
+        nlev = carray(nlev)
+        u = np.array(u) if not hasattr(u, 'ndim') else u
+        if not max(u.shape) == np.prod(u.shape):
+            warn("Multiple input delta sigma structures have had little testing.")
+        if u.ndim == 1:
+            u = u.reshape((1, -1))
+        ABCD = np.asarray(arg2, dtype=np.float64)
+        nq = 1 if np.isscalar(nlev) else nlev.shape[0]
+        if ABCD.shape[1] != ABCD.shape[0] + u.shape[0]:
+            raise ValueError('The ABCD argument does not have proper dimensions.')
+        if not isinstance(x0, collections.Iterable):
+            x0 = x0*np.ones((ABCD.shape[0] - nq, 1))
+        else:
+            x0 = np.array(x0).reshape((-1, 1))
+        print('u = {}'.format(u))
+        print('ABCD = {}'.format(ABCD))
+        print('nq = {}'.format(nq))
+        print('nlev = {}'.format(nlev))
+        print('x0 = {}'.format(x0))
+        v, xn, xmax, y = simulateDSM_ABCD(u, ABCD, nq, nlev, x0)
+        return v, xn, xmax, y
+    else:
+        raise ValueError("Stop trying to do things that aren't supported yet!")
 
-    nlev = carray(nlev)
-    u = np.array(u) if not hasattr(u, 'ndim') else u
-    if not max(u.shape) == np.prod(u.shape):
-        warn("Multiple input delta sigma structures have had little testing.")
-    if u.ndim == 1:
-        u = u.reshape((1, -1))
-    nu = u.shape[0]
-    nq = 1 if np.isscalar(nlev) else nlev.shape[0]
-    # extract poles and zeros
-
+@jit(argtypes=([double[:, :], double[:, :], int32, int32[:], double[:]]))
+#@autojit
+def simulateDSM_ABCD(u, ABCD, nq, nlev=2, x0=0):
     # Removing type checking that is conflicting with Numba
     #if (hasattr(arg2, 'inputs') and not arg2.inputs == 1) or \
     #   (hasattr(arg2, 'outputs') and not arg2.outputs == 1):
     #        raise TypeError("The supplied TF isn't a SISO transfer function.")
-    if isinstance(arg2, np.ndarray):
-        ABCD = np.asarray(arg2, dtype=np.float64)
-    #    if ABCD.shape[1] != ABCD.shape[0] + nu:
-    #        raise ValueError('The ABCD argument does not have proper dimensions.')
-        form = 1
-    else:
-        zeros, poles, k = _get_zpk(arg2)
-        form = 2
-    #raise TypeError('%s: Unknown transfer function %s' % (__name__, str(arg2)))
-
-    # need to set order and form now.
-    order = carray(zeros).shape[0] if form == 2 else ABCD.shape[0] - nq
-
-    if not isinstance(x0, collections.Iterable):
-        x0 = x0*np.ones((order, 1))
-    else:
-        x0 = np.array(x0).reshape((-1, 1))
-
-    if form == 1:
-        A = ABCD[:order, :order]
-        B = ABCD[:order, order:order+nu+nq]
-        C = ABCD[order:order+nq, :order]
-        D1 = ABCD[order:order+nq, order:order+nu]
-    else:
-        A, B2, C, D2 = zpk2ss(poles, zeros, -1)    # A realization of 1/H
-        # Transform the realization so that C = [1 0 0 ...]
-        C, D2 = np.real_if_close(C), np.real_if_close(D2)
-        Sinv = orth(np.hstack((np.transpose(C), np.eye(order)))) / norm(C)
-        S = inv(Sinv)
-        C = np.dot(C, Sinv)
-        if C[0, 0] < 0:
-            S = -S
-            Sinv = -Sinv
-        A = np.dot(np.dot(S, A), Sinv)
-        B2 = np.dot(S, B2)
-        C = np.hstack((np.ones((1, 1)), np.zeros((1, order-1)))) # C=C*Sinv;
-        D2 = np.zeros((0,))
-        # !!!! Assume stf=1
-        B1 = -B2
-        D1 = 1
-        B = np.hstack((B1, B2))
-
+    nu = u.shape[0]
+    order = ABCD.shape[0] - nq
+    A = ABCD[:order, :order]
+    B = ABCD[:order, order:order+nu+nq]
+    C = ABCD[order:order+nq, :order]
+    D1 = ABCD[order:order+nq, order:order+nu]
     N = u.shape[1]
     v = np.empty((nq, N), dtype=np.float64)
     y = np.empty((nq, N), dtype=np.float64)     # to store the quantizer input
--- deltasigma/numba_profile_script.py
+++ deltasigma/numba_profile_script.py
@@ -19,10 +19,11 @@
 import numpy as np
 from deltasigma import synthesizeNTF, realizeNTF, stuffABCD
 import _simulateDSM_numba
+import _simulateDSM_python
 import cProfile
 import pstats
 
-def test_script():
+def setup():
     OSR = 32
     H = synthesizeNTF(5, OSR, 1)
     N = 8192
@@ -30,15 +31,25 @@ def test_script():
     u = 0.5*np.sin(2*np.pi*f/N*np.arange(N))
     a, g, b, c = realizeNTF(H, 'CRFB')
     ABCD = stuffABCD(a, g, b, c, form='CRFB')
+    return OSR, H, N, f, u, ABCD
 
-    _simulateDSM_numba.simulateDSM(u, H, 2, 0)
+def test_script_numba():
+    OSR, H, N, f, u, ABCD = setup()
+    #_simulateDSM_numba.simulateDSM(u, H, 2, 0)
+    _simulateDSM_numba.simulateDSM(u, ABCD, 2, 0)
 
-        #_simulateDSM_numba.simulateDSM(u, ABCD, 2, 0)
+def test_script_cpython():
+    OSR, H, N, f, u, ABCD = setup()
+    #_simulateDSM_python.simulateDSM(u, H, 2, 0)
+    _simulateDSM_python.simulateDSM(u, ABCD, 2, 0)
 
-        #_simulateDSM_python.simulateDSM(u, H, 2, 0)
+cProfile.run('test_script_numba()', 'numba_stats')
+pn = pstats.Stats('numba_stats')
+print('Numba stats')
+pn.sort_stats('time').print_stats(10)
+pn.sort_stats('cumtime').print_stats(10)
 
-        #_simulateDSM_python.simulateDSM(u, ABCD, 2, 0)
-
-cProfile.run('test_script()', 'numba_stats')
-p = pstats.Stats('numba_stats')
-p.sort_stats('time').print_stats(25)
+cProfile.run('test_script_cpython()', 'cpython_stats')
+pc = pstats.Stats('cpython_stats')
+print('CPython stats')
+pc.sort_stats('time').print_stats(10)
