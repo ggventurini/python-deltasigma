300addcbde69a984999573fdbb16d0e2c6459110
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2022-01-07 19:24:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,10 +42,10 @@ setup(
         "Operating System :: MacOS",
         "Natural Language :: English",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ]
 )
 
